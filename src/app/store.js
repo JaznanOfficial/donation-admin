8145a2900ec749b80887a37d26aa6b415d5f1b2c
Md Abdur Rahman <82861990+JaznanOfficial@users.noreply.github.com>
2023-03-10 18:36:11 +0600

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import transactions from "../features/transaction/transactionSlice"
-
-export const store = configureStore({
-    reducer: {
-        transactions,
-    },
-});
